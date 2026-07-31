121b62cc7dec532a07aec0fd01980a4801c4620e
kelosocialeu <kelosocial.eu@gmail.com>
2026-07-31 16:55:18 +0200

    Create verification.js

---
+++ pages/verification.js
@@ -0,0 +1,13 @@
+// pages/verification.js
+import Layout from '../components/Layout';
+import VerificationForm from '../components/VerificationForm';
+
+export default function Verification() {
+  return (
+    <Layout>
+      <section className="py-20">
+        <VerificationForm />
+      </section>
+    </Layout>
+  );
+}
